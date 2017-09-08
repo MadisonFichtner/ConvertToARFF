f6e09d6c5452eac13911737adf066bf2c33ecfb2
Bryan Plant <bryanplant@gmail.com>
2017-09-08 17:37:24 -0600

    Works mostly
    It works on data files that don't already have a header
    Also the class has have words not numbers(e.g works with iris but not
    with the breast cancer one because the breast cancer one uses numbers
    for the class variables)

--- src/Converter.java
+++ src/Converter.java
@@ -8,62 +8,23 @@ import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
 
 public class Converter {
+	File file;
 	public void readURL(String link) throws IOException{
+		
+		file = new File("output.csv");
+		
+		dataToCsv(link);
+		csvToArff();
+	}
+	
+	public void dataToCsv(String link) throws IOException{
 		URL url = new URL(link);					//create a pointer to file on database
 		Scanner s = new Scanner(url.openStream());	//create a scanner with url
 		Scanner in = new Scanner(System.in);
-		PrintWriter writer = new PrintWriter("ouput.arff", "UTF-8");
-
-		System.out.println("What is the relation?");
-		writer.println("@RELATION " + in.nextLine());
-		writer.println();
-		System.out.println("How many attributes?");
-		int numAttributes = in.nextInt();
-		for(int i = 0; i < numAttributes; i++){
-			System.out.println("What is attribute " + (i+1) + "?");
-			String attribute = in.next();
-			System.out.println("What type is the attribute?(Not including class)");
-			System.out.println("\t1) NUMERIC");
-			System.out.println("\t2) INTEGER");
-			System.out.println("\t3) REAL");
-			System.out.println("\t4) STRING");
-			System.out.println("\t5) DATE");
-			int selection = in.nextInt();
-
-			String type = "NUMERIC";
-			switch(selection){
-			case 1:
-				type = "NUMERIC";
-				break;
-			case 2:
-				type = "INTEGER";
-				break;
-			case 3:
-				type = "REAL";
-				break;
-			case 4:
-				type = "STRING";
-				break;
-			case 5:
-				type = "DATE";
-				break;
-			}
-			writer.println("@ATTRIBUTE " + attribute + " "+ type);
-		}
-
-		System.out.println("How many classes are there?");
-		int numClasses = in.nextInt();
-		writer.print("@ATTRIBUTE CLASS {");
-		for(int i=0; i<numClasses; i++){
-			System.out.println("What is class " + (i+1) + "?");
-			writer.print(in.next());
-			if(i != numClasses -1)
-				writer.print(",");
-		}
-
-		writer.println("}");
-		writer.println();
-		writer.println("@DATA");
+		PrintWriter writer = new PrintWriter(file);
+		
+		System.out.println("What are the attributes?");
+		writer.println(in.next());
 
 		while(s.hasNext()){
 			writer.println(s.next());	//prints each line of data file to output file
@@ -71,22 +32,36 @@ public class Converter {
 		writer.close();
 		s.close();
 		in.close();
-
-		/*String f1 = "output.csv";
-        String f2 = "C:/Users/Bryan/Downloads/new.arff";
-
+	}
+	
+	public void csvToArff(){
+        String f2 = "output.arff";
+        
         try{
-        // load CSV
-        CSVLoader loader = new CSVLoader();
-	    loader.setSource(new File(f1));
-	    Instances data = loader.getDataSet();
-
-	    // save ARFF
-	    ArffSaver saver = new ArffSaver();
-	    saver.setInstances(data);
-	    saver.setFile(new File(f2));
-	    saver.writeBatch();
+        	
         }
-        catch(Exception e){}*/
+        catch(Exception e){
+        	e.printStackTrace();
+        }
+        
+        try {
+
+            // load the CSV file (input file)
+            CSVLoader loader = new CSVLoader();
+            loader.setSource(file);
+
+
+            Instances data = loader.getDataSet();
+            System.out.println(data);
+
+            // save as an  ARFF (output file)
+            ArffSaver saver = new ArffSaver();
+            saver.setInstances(data);
+            saver.setFile(new File(f2));
+            //saver.setDestination(new File(f2));
+            saver.writeBatch();
+        } catch(Exception e) {
+        	e.printStackTrace();
+        }
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,9 @@ public class Main {
 	public static void main(String[] args){
 		Converter converter = new Converter();
 		try {
-			converter.readURL("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data");
+			converter.readURL("http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		/*String f1 = "C:/Users/Bryan/Downloads/iris.csv";
-        String f2 = "C:/Users/Bryan/Downloads/iris.arff";
-        
-        try{
-        // load CSV
-        CSVLoader loader = new CSVLoader();
-	    loader.setSource(new File(f1));
-	    Instances data = loader.getDataSet();
-	   
-	    // save ARFF
-	    ArffSaver saver = new ArffSaver();
-	    saver.setInstances(data);
-	    saver.setFile(new File(f2));
-	    //saver.setDestination(new File(f2));
-	    saver.writeBatch();
-        }
-        catch(Exception e){}*/
 	}
 }
