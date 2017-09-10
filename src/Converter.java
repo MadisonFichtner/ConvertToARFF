3fff702281e31b0bf19ce273d922a6916d5faed7
Bryan Plant <bryanplant@gmail.com>
2017-09-10 11:57:03 -0600

    User can input data with file or url

--- src/Converter.java
+++ src/Converter.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
@@ -8,21 +10,40 @@ import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
 
 public class Converter {
-	File file;
-	public void readURL(String link) throws IOException{
-		
-		file = new File("output.csv");
-		
-		dataToCsv(link);
-		csvToArff();
+
+	File dataFile;
+
+	public void getDataFile(String filename){
+		try {
+			dataFile = new File(filename);
+			dataToCsv(dataFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public void dataToCsv(String link) throws IOException{
+
+	public void readURL(String link) throws IOException{
 		URL url = new URL(link);					//create a pointer to file on database
 		Scanner s = new Scanner(url.openStream());	//create a scanner with url
+		File data = new File("temp.data");
+		PrintWriter writer = new PrintWriter(data);
+
+		while(s.hasNext()){
+			writer.println(s.next());	//prints each line of data file to output file
+		}
+		s.close();
+		writer.close();
+
+		dataToCsv(data);
+	}
+
+	public void dataToCsv(File data) throws IOException{
+		File csvFile = new File("output.csv");
+		PrintWriter writer = new PrintWriter(csvFile);
+		BufferedReader reader = new BufferedReader(new FileReader(data));
+
 		Scanner in = new Scanner(System.in);
-		PrintWriter writer = new PrintWriter(file);
-		
+
 		System.out.println("How many attributes? (Not including class)");
 		int numAttributes = in.nextInt();
 		for(int i=0; i<numAttributes; i++){
@@ -31,30 +52,33 @@ public class Converter {
 		}
 		writer.println("Class");
 
-		while(s.hasNext()){
-			writer.println(s.next());	//prints each line of data file to output file
+		String line;
+		while((line = reader.readLine()) != null){
+			writer.println(line);
 		}
-		
+
 		writer.close();
-		s.close();
 		in.close();
+
+
+		csvToArff(csvFile);
 	}
-	
-	public void csvToArff(){
-        String f2 = "output.arff";
-        
+
+	public void csvToArff(File csvFile){
+        File arffFile = new File("output.arff");
+
         try{
-        	
+
         }
         catch(Exception e){
         	e.printStackTrace();
         }
-        
+
         try {
 
             // load the CSV file (input file)
             CSVLoader loader = new CSVLoader();
-            loader.setSource(file);
+            loader.setSource(csvFile);
 
 
             Instances data = loader.getDataSet();
@@ -63,7 +87,7 @@ public class Converter {
             // save as an  ARFF (output file)
             ArffSaver saver = new ArffSaver();
             saver.setInstances(data);
-            saver.setFile(new File(f2));
+            saver.setFile(arffFile);
             //saver.setDestination(new File(f2));
             saver.writeBatch();
         } catch(Exception e) {
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args){
 		Converter converter = new Converter();
-		try {
-			converter.readURL("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data");
-		} catch (IOException e) {
-			e.printStackTrace();
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Do you want to convert data from a file(Enter '1') or from a URL(Enter '2')?");
+		int selection = in.nextInt();
+
+		if(selection == 1){
+			System.out.println("Enter the name of the file int the project folder");
+			converter.getDataFile(in.next());
 		}
+		else{
+			try {
+				System.out.println("Enter the URL to the data file.");
+				converter.readURL(in.next());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		in.close();
 	}
 }
