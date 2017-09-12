26e6fd77893c1d1c30bf7eab86f4a7668347908c
Brick7Face <Brick7Face@users.noreply.github.com>
2017-09-12 17:39:01 -0600

    user error catching for most things, I'll add comments here soon

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -6,23 +7,45 @@ public class Main {
 		Converter converter = new Converter();
 		Scanner in = new Scanner(System.in);
 
-		System.out.println("Do you want to convert data from a file(Enter '1') or from a URL(Enter '2')?");
-		int selection = in.nextInt();
-		switch (selection) {
-			case 1: {
-				System.out.println("Enter the name of the file in the project folder");
-				converter.getDataFile(in.next());
+		int selection = 0;
+		boolean valid = true;
+		do {	
+			System.out.println("Do you want to convert data from a file(Enter '1') or from a URL(Enter '2')?");
+			try {
+				selection = in.nextInt();
 			}
-			case 2: {
-				try {
-					System.out.println("Enter the URL to the data file.");
-					converter.readURL(in.next());
-				} catch (IOException e) {
-					System.out.println("That did not work. Check URL and try again.");
+			catch (Exception e) {
+				System.out.printf("Invalid input.\n\n");
+				in.nextLine();
+				valid = false;
+				continue;
+			}
+			switch (selection) {
+				case 1: {
+					System.out.println("Enter the name of the file in the project folder");
+					converter.getDataFile(in.next());
+					break;
+				}
+				case 2: {
+					boolean valid1 = true;
+					do {
+						try {
+							System.out.println("Enter the URL to the data file.");
+							converter.readURL(in.next());
+						} catch (IOException e) {
+							System.out.printf("That did not work. Check URL and try again.\n\n");
+							in.nextLine();
+							valid1 = false;
+						}
+					} while (valid1 == false);
+					break;
+				}
+				default: {
+					System.out.printf("Invalid number.\n\n");
+					valid = false;
 				}
 			}
-			default: System.out.println("Incorrect input.");
-		}
+		} while (valid == false);
 		in.close();
 	}
 }
