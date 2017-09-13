58bd95fb894bbfada8430ebec5cf8798f4402612
Bryan Plant <bryanplant@gmail.com>
2017-09-13 17:21:04 -0600

    No longer uses WEKA helper functions

--- src/Converter.java
+++ src/Converter.java
@@ -6,16 +6,14 @@ import java.io.PrintWriter;
 import java.net.URL;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import weka.core.Instances;
-import weka.core.converters.ArffSaver;
-import weka.core.converters.CSVLoader;
+
 
 public class Converter {
 
 	File dataFile;
+	Scanner in = new Scanner(System.in);
 
 	public void getDataFile(){
-		Scanner in = new Scanner(System.in);
 		boolean valid = true;
 		do {
 			System.out.print("Please enter the name of a file located in the project folder: ");
@@ -29,7 +27,6 @@ public class Converter {
 				valid = false;
 			}
 		} while (valid == false);
-		in.close();
 	}
 
 	public void readURL(String link) throws IOException {
@@ -59,7 +56,6 @@ public class Converter {
 			System.out.println("Something went wrong generating output file.");		//fix this --> test 1, filename = asdf
 		}
 
-		Scanner in = new Scanner(System.in);
 		int numAttributes = 0;
 		boolean valid = true;
 		do {
@@ -89,7 +85,6 @@ public class Converter {
 		}
 
 		writer.close();
-		in.close();
 		try {
 			reader.close();
 		} catch (Exception e) {
@@ -97,33 +92,83 @@ public class Converter {
 		}
 
 
-		csvToArff(csvFile);
+		csvToArff(csvFile, numAttributes);
 	}
 
-	public void csvToArff(File csvFile){
+	public void csvToArff(File csvFile, int numAttributes){
         File arffFile = new File("output.arff");
+        Scanner s = null;
+        PrintWriter writer = null;
 
-        try {
-
-            // load the CSV file (input file)
-            CSVLoader loader = new CSVLoader();
-            loader.setSource(csvFile);
-            String[] options = new String[2];
-            options[0] = "-N";					//forces an attribute to be nominal type
-            options[1] = "last";				//forces the last attribute(class) to be nominal
-            loader.setOptions(options);
-
-            Instances data = loader.getDataSet();
-            System.out.println("Finished writing data to file.");
-
-            // save as an  ARFF (output file)
-            ArffSaver saver = new ArffSaver();
-            saver.setInstances(data);
-            saver.setFile(arffFile);
-            //saver.setDestination(new File(f2));
-            saver.writeBatch();
-        } catch(Exception e) {
+        try{
+	        s = new Scanner(csvFile);
+			writer = new PrintWriter(arffFile);
+        }
+        catch(Exception e){
         	e.printStackTrace();
         }
+        
+        System.out.println("What is the relation?");
+		writer.print("@RELATION ");
+		writer.println(in.next());
+		writer.println();
+        
+        s.useDelimiter(",");
+        for(int i = 0; i < numAttributes; i++){
+        	writer.print("@ATTRIBUTE ");
+        	writer.print(s.next() + " ");
+			System.out.println("What type is attribute " + (i+1) + "?");
+			System.out.println("\t1) NUMERIC");
+			System.out.println("\t2) INTEGER");
+			System.out.println("\t3) REAL");
+			System.out.println("\t4) STRING");
+			System.out.println("\t5) DATE");
+
+			in.nextLine();
+			int selection = in.nextInt();
+
+			String type = "NUMERIC";
+			switch(selection){
+			case 1:
+				type = "NUMERIC";
+				break;
+			case 2:
+				type = "INTEGER";
+				break;
+			case 3:
+				type = "REAL";
+				break;
+			case 4:
+				type = "STRING";
+				break;
+			case 5:
+				type = "DATE";
+				break;
+			}
+			writer.println(type);
+        }
+        System.out.println("How many classes are there?");
+		int numClasses = in.nextInt();
+		writer.print("@ATTRIBUTE CLASS {");
+		for(int i=0; i<numClasses; i++){
+			System.out.println("What is class " + (i+1) + "?");
+			writer.print(in.next());
+			if(i != numClasses -1)
+				writer.print(",");
+		}
+		writer.println("}");
+		writer.println();
+        
+        s.useDelimiter(" ");
+        s.skip(",Class");
+        
+		writer.print("@DATA");
+
+		while(s.hasNext()){
+			writer.println(s.next());	//prints each line of data file to output file
+		}
+		writer.close();
+		s.close();
+		in.close();
 	}
 }
